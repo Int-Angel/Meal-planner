32f42013bdd6cb8c8b2158cef0ab88af3d3e0c33
Int-Angel <Padilla.angel88@gmail.com>
2021-07-12 21:46:02 -0500

    Recipes and week header Basic navigation

--- MealPlanner/app/src/main/java/com/example/mealplanner/MainActivity.java
+++ MealPlanner/app/src/main/java/com/example/mealplanner/MainActivity.java
@@ -7,6 +7,9 @@ import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RelativeLayout;
 
 import com.example.mealplanner.fragments.OnlineRecipesFragment;
 import com.example.mealplanner.fragments.ProfileFragment;
@@ -18,6 +21,10 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    enum FragmentSelection {
+        WEEK, SHOPPING_LIST, SAVED_RECIPES, ONLINE_RECIPES, SOCIAL, PROFILE;
+    }
 
     private final static String TAG = "MainActivity";
 
@@ -31,41 +38,114 @@ public class MainActivity extends AppCompatActivity {
 
     private BottomNavigationView bottomNavigationView;
 
+    private RelativeLayout recipeFragmentHeader;
+    private RelativeLayout weekFragmentHeader;
+
+    private FragmentSelection activeFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        weekFragmentHeader = findViewById(R.id.weekFragmentHeader);
+        recipeFragmentHeader = findViewById(R.id.recipeFragmentHeader);
+        recipeFragmentHeader.setVisibility(View.GONE);
+
         bottomNavigationView = findViewById(R.id.bottomNavigation);
         setBottomNavigationListener();
         bottomNavigationView.setSelectedItemId(R.id.action_week);
+        activeFragment = FragmentSelection.WEEK;
     }
 
     private void setBottomNavigationListener() {
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                changeFragment(item);
+                changeFragment(getFragmentSelectionFromMenu(item));
                 return true;
             }
         });
     }
 
-    private void changeFragment(MenuItem item) {
-        Fragment fragment;
+    public void onRadioButtonClickedRecipes(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+        switch(view.getId()) {
+            case R.id.rbSavedRecipes:
+                if (checked)
+                    changeFragment(FragmentSelection.SAVED_RECIPES);
+                    break;
+            case R.id.rbOnlineRecipes:
+                if (checked)
+                    changeFragment(FragmentSelection.ONLINE_RECIPES);
+                    break;
+        }
+    }
 
+    public void onRadioButtonClickedWeek(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+        switch(view.getId()) {
+            case R.id.rbWeek:
+                if (checked)
+                    changeFragment(FragmentSelection.WEEK);
+                break;
+            case R.id.rbShoppingList:
+                if (checked)
+                    changeFragment(FragmentSelection.SHOPPING_LIST);
+                break;
+        }
+    }
+
+    private FragmentSelection getFragmentSelectionFromMenu(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_week:
-                fragment = weekFragment;
-                break;
+                return FragmentSelection.WEEK;
             case R.id.action_recipes:
-                fragment = savedRecipesFragment;
-                break;
+                return FragmentSelection.SAVED_RECIPES;
             case R.id.action_social:
-                fragment = socialFragment;
-                break;
+                return FragmentSelection.SOCIAL;
             case R.id.action_profile:
             default:
+                return FragmentSelection.PROFILE;
+        }
+    }
+
+    private void changeFragment(FragmentSelection fragmentSelected) {
+        if(activeFragment == fragmentSelected) return;
+
+        Fragment fragment;
+        activeFragment = fragmentSelected;
+
+        switch (fragmentSelected) {
+            case WEEK:
+                weekFragmentHeader.setVisibility(View.VISIBLE);
+                recipeFragmentHeader.setVisibility(View.GONE);
+                fragment = weekFragment;
+                break;
+            case SHOPPING_LIST:
+                weekFragmentHeader.setVisibility(View.VISIBLE);
+                recipeFragmentHeader.setVisibility(View.GONE);
+                fragment = shoppingListFragment;
+                break;
+            case SAVED_RECIPES:
+                weekFragmentHeader.setVisibility(View.GONE);
+                recipeFragmentHeader.setVisibility(View.VISIBLE);
+                fragment = savedRecipesFragment;
+                break;
+            case ONLINE_RECIPES:
+                weekFragmentHeader.setVisibility(View.GONE);
+                recipeFragmentHeader.setVisibility(View.VISIBLE);
+                fragment = onlineRecipesFragment;
+                break;
+            case SOCIAL:
+                weekFragmentHeader.setVisibility(View.GONE);
+                recipeFragmentHeader.setVisibility(View.GONE);
+                fragment = socialFragment;
+                break;
+            case PROFILE:
+            default:
+                weekFragmentHeader.setVisibility(View.GONE);
+                recipeFragmentHeader.setVisibility(View.GONE);
                 fragment = profileFragment;
                 break;
         }
